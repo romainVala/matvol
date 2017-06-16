c4608433100124928205a79222e9e768af0d4020
= <benoit.beranger@icm-institute.org>
2017-06-16 20:30:22 +0200

    cleaned addsuffixtofilenames

---
+++ Tools/files_and_dir/addsuffixtofilenames.m
@@ -0,0 +1,79 @@
+function newlist = addsuffixtofilenames(filelist,suffix)
+% ADDSUFIXTOFILENAMES Adds suffixs to files name.
+%
+% 1. filelist can be a cell-array where each element is a char-array
+% containing a list of file names.
+%
+% 2. filelist can be a char-array containing a list of file names.
+%
+% suffix can be char : suffix will be applied to each elements of the
+% filelist{i}.
+%
+% suffix can be cell-array of char : suffix{i} will be applied to each
+% elements of the filelist{i}.
+%
+% note : the 'filelist' will remain a char or a cell, depending on its
+% nature.
+%
+
+%% Check input arguments
+
+if nargin ~= 2
+    error('filelist & suffix must be defined')
+end
+
+
+%% Prepare inputs and outputs format
+
+suffix = cellstr(suffix); % avoid problems of class or dimensions
+
+% If the filelist is char, we want the newlist to also be char
+makeitchar = 0;
+if ischar(filelist)
+    filelist   = cellstr(filelist);
+    makeitchar = 1;
+end
+
+% Repeat suffix to match filelist size if needed
+if numel(suffix) == 1
+    suffix = repmat(suffix,size(filelist));
+end
+
+if numel(filelist) > numel(suffix)
+    error('Dimensions mismatch : if there is N elements in filelist, there must be N suffix, OR juste 1 suffix')
+end
+
+
+%% Add the suffixs
+
+newlist = cell(1,length(filelist));
+
+for i=1:length(filelist)
+    
+    wfiles = [];
+    
+    for line = 1:size(filelist{i},1)
+        
+        [pathstr,name,extension] = fileparts(deblank(filelist{i}(line,:)));
+        
+        %for double extention (.nii.gz)
+        [~,nm2,xt2] = fileparts(name);
+        if ~isempty(xt2)
+            extension = [xt2 extension]; %#ok<AGROW>
+            name = nm2;
+        end
+        
+        
+        wfiles  = [wfiles ; fullfile(pathstr,[name suffix{i} extension ])]; %#ok<AGROW>
+        
+    end
+    
+    newlist{i} = wfiles;
+    
+end
+
+if makeitchar
+    newlist = char(newlist);
+end
+
+end % function
