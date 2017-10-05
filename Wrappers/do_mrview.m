aa59706f98476f719941a364af152b7f1b7b9971
valabregue <romain.valabregue@upmc.fr>
2017-10-05 16:23:24 +0200

    with vglrun and 2nd argument is overlay

--- Wrappers/do_mrview.m
+++ Wrappers/do_mrview.m
@@ -1,16 +1,23 @@
-function mrview(f)
+function mrview(f,fref)
+if ~exist('fref','var')
+    fref='';
+end
 
 f=cellstr(char(f));
 
-cmd='mrview2';
+cmd='mrviewv';
 %cmd='fslview';
 
 for k=1:length(f)
     cmd = sprintf('%s %s',cmd,f{k});
 end
 
+if ~isempty(fref)
+    fref=char(fref);
+cmd = sprintf('%s -overlay.load %s -overlay.opacity 0.3 &',cmd,fref)
+else
 cmd = sprintf('%s &',cmd)
-
+end
 
 unix(cmd)
 
