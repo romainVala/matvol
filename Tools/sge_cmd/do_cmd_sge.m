06fd7f9f93779081bf2b4ac9ba9e4ef9bcea2946
valabregue <romain.valabregue@upmc.fr>
2017-10-09 23:52:13 +0200

    add par.parallel use

--- Tools/sge_cmd/do_cmd_sge.m
+++ Tools/sge_cmd/do_cmd_sge.m
@@ -12,7 +12,7 @@ def_par.software_path = '';
 
 def_par.jobdir=pwd;
 def_par.sge=1;
-def_par.sge_queu = 'short';
+def_par.sge_queu = 'normal';
 def_par.job_append = 1;
 def_par.sge_nb_coeur=1;
 def_par.submit_sleep = 1;  %add a sleep of 1 second between each qsub
@@ -23,8 +23,13 @@ def_par.mem = 4000;  %give a number in Mega --mem=[mem][M|G|T] OR --mem-per-cpu=
 def_par.job_pack = 1;
 def_par.sbatch_args = '--export=NONE -m block:block ';
 def_par.jobappend = '';
+def_par.parallel=0;
 
 par = complet_struct(par,def_par);
+
+if par.parallel>0
+    par.sge_nb_coeur=par.parallel;
+end
 
 if isempty(jobappend) %changing convention jobappend part of the structure
     jobappend = par.jobappend;
@@ -143,27 +148,29 @@ else
             cmdd = sprintf('%s\n%s',par.software_path,cmdd);
         end
         
-        jname = sprintf('j%.2d_%s',k+kinit,par.jobname);
-        
+        jname = sprintf('j%.2d_%s',k+kinit,par.jobname);        
         fpn = fullfile(job_dir,jname);
-        fpnlog = sprintf('%s.log',fpn);
-        fpnlogerror = sprintf('%s.err',fpn);
+        fpnlog = sprintf('%s.log',fpn);        fpnlogerror = sprintf('%s.err',fpn);
+        
+        if par.parallel>0
+            nbpara = ceil((length(job)+kinit)/par.parallel);
+            k_para =ceil((k+kinit)/par.parallel);            
+            para_jname = sprintf('p%.2d_%s',k_para,par.jobname);
+            fpara = fullfile(job_dir,para_jname);            
+            ffpara = fopen(fpara,'a+');
+            fprintf(ffpara,'bash %s > lopara_%s 2> erpara_%s \n',fpn,jname,jname);
+            fclose(ffpara);
+        end
         
         ff=fopen(fpn,'w');
         switch par.sge_queu
             case {'server_ondule','server_irm'}
                 fprintf(ff,'#$ -S /bin/bash \n');
-                fprintf(ff,cmdd);
-                fclose(ff);
-                
             otherwise
                 fprintf(ff,'#!/bin/bash\n');
-                
-                %fprintf(ff,'',);
-                fprintf(ff,cmdd);
-                
-                fclose(ff);
         end
+        fprintf(ff,cmdd);
+        fclose(ff);
         
         switch par.sge_queu
             case {'server_ondule','server_irm'}
@@ -188,11 +195,16 @@ else
     if ~isempty(qsubappend),        fprintf(fqsubar,'  --depend=afterok:$jobid ');    end
     
     fprintf(fqsubar,' %s ',par.sbatch_args);
-    
-    if ~isempty(qsubappend)
-        fprintf(fqsubar,' -o %s/log-%%A_%%a  -e %s/err-%%A_%%a  --array=1-%d %s |awk ''{print $4}''` \necho submitted job $jobid\n',job_dir,job_dir,k+kinit,f_do_array);
+    if par.parallel
+        nb_job=nbpara;
     else
-        fprintf(fqsubar,' -o %s/log-%%A_%%a  -e %s/err-%%A_%%a  --array=1-%d %s |awk ''{print $4}''` \necho submitted job $jobid\n',job_dir,job_dir,k+kinit,f_do_array);
+        nb_job=k+kinit;
+    end
+
+    if ~isempty(qsubappend)
+        fprintf(fqsubar,' -o %s/log-%%A_%%a  -e %s/err-%%A_%%a  --array=1-%d %s |awk ''{print $4}''` \necho submitted job $jobid\n',job_dir,job_dir,nb_job,f_do_array);
+    else
+        fprintf(fqsubar,' -o %s/log-%%A_%%a  -e %s/err-%%A_%%a  --array=1-%d %s |awk ''{print $4}''` \necho submitted job $jobid\n',job_dir,job_dir,nb_job,f_do_array);
     end
     fclose(fqsubar);
     
@@ -201,7 +213,12 @@ else
 	fprintf(fffa,'\n\necho started on $HOSTNAME \n date\n\n');
     fprintf(fffa,'tic="$(date +%%s)"\n\n');
     
-    fprintf(fffa,' cmd=$( printf "j%%02d_%s" ${SLURM_ARRAY_TASK_ID})\n bash %s/$cmd\n\n',par.jobname,job_dir);
+    if par.parallel 
+        fprintf(fffa,' cmd=$( printf "p%%02d_%s" ${SLURM_ARRAY_TASK_ID})\n parallel -j %d < %s/$cmd\n\n',par.jobname,par.parallel,job_dir);
+    else
+        fprintf(fffa,' cmd=$( printf "j%%02d_%s" ${SLURM_ARRAY_TASK_ID})\n bash %s/$cmd\n\n',par.jobname,job_dir);
+    end
+    
     fprintf(fffa,'\n\ntoc="$(date +%%s)";\nsec="$(expr $toc - $tic)";\nmin="$(expr $sec / 60)";\nheu="$(expr $sec / 3600)";\necho Elapsed time: $min min $heu H\n');
     
     % seff does not work because the job is still runing
