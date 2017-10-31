2f44572accddeaed0d78fa0913a955ce31d50011
valabregue <romain.valabregue@upmc.fr>
2017-10-31 18:22:13 +0100

    make it sge

--- Tools/nifti_hdr/gzip_volume.m
+++ Tools/nifti_hdr/gzip_volume.m
@@ -1,21 +1,36 @@
 function fo = gzip_volume(f)
 
+if ~exist('par','var'),par ='';end
+defpar.sge=0;
+defpar.jobname='zip'
+
+
+par = complet_struct(par,defpar);
+
 if isempty(f)
     return
 end
 
 f = cellstr(char(f));
-
+ind_to_remove=[];
+cmd = cell(size(f));
 for i=1:length(f)
 
   if ~strcmp(f{i}(end-1:end),'gz')
-    cmd = sprintf('gzip -f %s',f{i});
+    cmd{i} = sprintf('gzip -f %s',f{i});
 
     fo{i} = [f{i} '.gz'];
   
     unix(cmd);
   else
     fo{i} = f{i};
+    ind_to_remove(end+1)=i;
   end
   
+end
+
+cmd(ind_to_remove)=[];
+
+if ~isempty(cmd)
+    do_cmd_sge(cmd,par);
 end
