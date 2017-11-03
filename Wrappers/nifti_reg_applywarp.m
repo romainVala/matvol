a21ff654a7d7adc7df56bcdf9a2dee4e9c70dc38
valabregue <romain.valabregue@upmc.fr>
2017-11-03 12:27:30 +0100

    add the possibility to inverse the transfo (and delete it) and work in tmp dir

--- Wrappers/nifti_reg_applywarp.m
+++ Wrappers/nifti_reg_applywarp.m
@@ -1,5 +1,6 @@
 function nifti_reg_applywarp(fmov,fwarp,fref,par)
-
+%nifti_reg_applywarp(fmov,fwarp,fref,par) 
+% fmov images to apply transfo fwarp to reference image fref
 
 if ~exist('par','var'),par ='';end
 
@@ -10,7 +11,10 @@ defpar.prefix = 'nw_';
 defpar.mask = '';
 defpar.interp = 1; % Interpolation order (0, 1, 3, 4)[3] (0=NN, 1=LIN; 3=CUB, 4=SINC) 
 defpar.folder = 'warp'; % create res in folder where the warp is if = 'mov' 
-defpar.inv = 0;
+defpar.inv = 0;   
+defpar.inv_ref=''; % target image in the direct registration
+defpar.inv_temp_dir = ''; %
+defpar.inv_delete=0;
 
 par = complet_struct(par,defpar);
 
@@ -34,8 +38,19 @@ switch par.folder
 
 end
 
+%fwarpi = nifti_reg_inversewarp(fwarp,fanat,fref,par);  nifti_reg_inversewarp(fwarp,fref,frefw,par)
+%nifti_reg_applywarp(fmov,fwarpic,fanat,par)
+
 % -i ../../../template/mask.nii -r v10_s20141028_SN_Track_Rat1-917505-00001-000001.nii.gz -o iwmask.nii
 %-t [aw_v10_s20141028_SN_Track_Rat1-917505-00001-000001_to_ants_template0GenericAffine.mat,1] -t aw_v10_s20141028_SN_Track_Rat1-917505-00001-000001_to_ants_template1InverseWarp.nii.gz
+[dirw, fwarp_file] = get_parent_path(fwarp);
+        
+if par.inv
+    fo_def = addprefixtofilenames(fwarp_file,'defo_');
+    foinv = addprefixtofilenames(fwarp,'i');
+    frefiw = par.inv_ref;
+    if length(frefiw)==1,    frefiw = repmat(frefiw,size(fwarp)); end
+end
 
 job={};
 for k=1:length(fmov)
@@ -48,16 +63,32 @@ for k=1:length(fmov)
     end
     
     for nb_mov = 1:length(ffname_mov)
+        if isempty(par.inv_temp_dir)
+            cmd = sprintf('cd %s\n',dirw{k});
+        else
+            cmd = sprintf('cd %s\n',par.temp_dir);
+        end
+        
+        the_fwarp = fwarp{k};
+        if par.inv
+            cmd = sprintf('%s reg_transform -def %s %s -ref %s \n',cmd,the_fwarp,fo_def{k},fref{k});
+            cmd = sprintf('%s reg_transform -invNrr %s %s %s \n',cmd,fo_def{k},frefiw{k},foinv{k});  
+            the_fwarp = foinv{k};
+        end
         
         fo = fullfile(path_warp{nb_mov},[par.prefix ffname_mov{nb_mov} '.nii.gz']);
         
         %cmd = sprintf('cd %s',ores{k});
-        cmd = sprintf('reg_resample -flo %s -ref %s -res %s ',ffmov{nb_mov},fref{k},fo);
+        cmd = sprintf('%s reg_resample -flo %s -ref %s -res %s ',cmd,ffmov{nb_mov},fref{k},fo);
         
         cmd = sprintf('%s ',cmd);        
-        cmd = sprintf('%s -trans %s',cmd,fwarp{k});        
+        cmd = sprintf('%s -trans %s',cmd,the_fwarp);        
         cmd = sprintf('%s -inter %d \n',cmd,par.interp);
-                
+        
+        if par.inv_delete
+            cmd = sprintf('%s rm -f %s %s \n',cmd,fo_def{k},foinv{k})
+        end
+        
         job{end+1} = cmd;
     end
 end
