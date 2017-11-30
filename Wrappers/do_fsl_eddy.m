44611c3c556e29106f31a365b6285c44aada5dbb
valabregue <romain.valabregue@upmc.fr>
2017-11-30 12:01:20 +0100

    add a eddy version without topup (if par.topup is empty)

--- Wrappers/do_fsl_eddy.m
+++ Wrappers/do_fsl_eddy.m
@@ -28,26 +28,34 @@ if ~iscell(par.mask)
 end
 par.bvecs = get_subdir_regex_files(dtidir,par.bvecs,1);
 par.bvals = get_subdir_regex_files(dtidir,par.bvals,1);
-
-if ischar(par.topup_dir) % then it is relative to DWI dir
-    
-    par.topup_dir = addsuffixtofilenames(dtidir,['/' par.topup_dir]);
-end
-
-
-par.topup_acqp = addsuffixtofilenames(par.topup_dir,['/' par.topup_acqp]);
-if ~strcmp(par.topup(1),'/')
-    par.topup = addsuffixtofilenames(par.topup_dir,['/' par.topup]);
-end
-
 outfile = addsuffixtofilenames(f4D,par.outsuffix)
 
-for k=1:length(f4D)
+if isempty(par.topup)
+    for k=1:length(f4D)
+        acqp = get_subdir_regex_files(dtidir,'acqp',1);
+        
+        cmd = sprintf('eddy %s --imain=%s  --mask=%s  --index=%s  --bvecs=%s  --bvals=%s  --acqp=%s   --out=%s --resamp=%s\n',...
+            par.eddy_add_cmd,f4D{k},par.mask{k},par.index{k},par.bvecs{k},par.bvals{k},acqp{1},outfile{k},par.resamp)
+        
+        job{k} = cmd;
+    end
+
+else    
+    if ischar(par.topup_dir) % then it is relative to DWI dir
+        par.topup_dir = addsuffixtofilenames(dtidir,['/' par.topup_dir]);
+    end
+        
+    par.topup_acqp = addsuffixtofilenames(par.topup_dir,['/' par.topup_acqp]);
+    if ~strcmp(par.topup(1),'/')
+        par.topup = addsuffixtofilenames(par.topup_dir,['/' par.topup]);
+    end    
     
-    cmd = sprintf('eddy %s --imain=%s  --mask=%s  --index=%s  --bvecs=%s  --bvals=%s  --acqp=%s  --topup=%s  --out=%s --resamp=%s\n',...
-        par.eddy_add_cmd,f4D{k},par.mask{k},par.index{k},par.bvecs{k},par.bvals{k},par.topup_acqp{k},par.topup{k},outfile{k},par.resamp)
-    
-    job{k} = cmd;
+    for k=1:length(f4D)        
+        cmd = sprintf('eddy %s --imain=%s  --mask=%s  --index=%s  --bvecs=%s  --bvals=%s  --acqp=%s  --topup=%s  --out=%s --resamp=%s\n',...
+            par.eddy_add_cmd,f4D{k},par.mask{k},par.index{k},par.bvecs{k},par.bvals{k},par.topup_acqp{k},par.topup{k},outfile{k},par.resamp)
+        
+        job{k} = cmd;
+    end
 end
 
 job = do_cmd_sge(job,par,jobappend);
