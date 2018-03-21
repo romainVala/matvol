79d484f728537cb46c6ae100337e7a3db83a29cd
Benoît Béranger <benoit.beranger@icm-institute.org>
2018-03-21 13:00:56 +0100

    enhancement of @exam/removeIncomplete : can do depp copy (or not) and verbos option

--- objects/@exam/removeIncomplete.m
+++ objects/@exam/removeIncomplete.m
@@ -1,8 +1,11 @@
-function [ completeExams, incompleteExams ] = removeIncomplete( examArray )
-% REMOVEINCOMPLETE method removes incomplet @exams accoding to their flag .is_complete
-% IMPORTANT : This method requires an output argument, and will not affect the input
-% (due to MATLAB object behaviour).
-% Also, you can keep the incomplete exams (second output).
+function [ completeExams, incompleteExams ] = removeIncomplete( examArray, do_deep_copy, verbose )
+% REMOVEINCOMPLETE method sorts out complete and incomplete @exams according to their flag .is_complete
+%
+% SYNTAX : [ completeExams, incompleteExams ] = removeIncomplete( examArray, do_deep_copy=0/1, verbose=0/1 )
+%
+% VERY IMPORTANT : use 'do_deep_copy' flag to make deep copy of object (or not)
+% Note : This method requires an output argument.
+%
 
 AssertIsExamArray(examArray);
 
@@ -10,17 +13,44 @@ if nargout < 1
     error('[%s]: At least one output argument is required', mfilename)
 end
 
-completeExams   = examArray;  % NOT a deep copy of the array, just pointer copy
-incompleteExams = exam.empty; % empty array
-
-for ex = length(examArray) : -1 : 1
-    if examArray(ex).is_incomplete
-        fprintf('\n')
-        fprintf('[%s]: The exam #%d will be removed : \n', mfilename, ex)
-        examArray(ex).explore
-        completeExams(ex) = [];
-        incompleteExams(end+1,1) = examArray(ex); %#ok<AGROW>
-    end
+if nargin < 3
+    verbose = 0;
 end
+
+if nargin < 2
+    do_deep_copy = 1;
+end
+
+
+%% Sort out
+
+flags = {examArray.is_incomplete}; % { [] []  1 [] ...}
+where = cellfun(@isempty, flags);  % [  1  1  0  1 ...]
+
+if do_deep_copy
+    
+    % Deep copy
+    completeExams   = examArray.copyObject;
+    incompleteExams = examArray.copyObject;
+    
+else
+    
+    % Pointer copy
+    completeExams   = examArray;
+    incompleteExams = examArray;
+    
+end
+
+% Sort out
+completeExams   = completeExams  ( where);
+incompleteExams = incompleteExams(~where);
+
+if verbose
+    fprintf('\n')
+    fprintf('[%s]: The exam(s) bellow will be removed : \n', mfilename)
+    disp(find(~where))
+    incompleteExams.explore
+end
+
 
 end % function
