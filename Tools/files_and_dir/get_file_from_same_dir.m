70d97f1d5e533b8a6996b1a42588f399395fd3ac
valabregue <romain.valabregue@upmc.fr>
2018-04-04 16:25:52 +0200

    add nb of file argument

--- Tools/files_and_dir/get_file_from_same_dir.m
+++ Tools/files_and_dir/get_file_from_same_dir.m
@@ -1,7 +1,15 @@
-function B = get_file_from_same_dir(A,B)
+function B = get_file_from_same_dir(A,B,p)
 %for a given cell list of infile A : construct a file list from the same dir  
 % of A with regulare expression given by B 
 %if B is a cell list (already set by the user) just check the length is the same as A
+
+if ~exist('p'), p=struct;end
+
+if isnumeric(p)
+    aa=p;clear p
+    p.wanted_number_of_file = aa;
+    p.verbose=0;
+end
 
 if iscell(B)
     if length(B)==length(A)
@@ -12,4 +20,4 @@ if iscell(B)
 end
 
 dir = get_parent_path(A,1);
-B = get_subdir_regex_files(dir,B);
+B = get_subdir_regex_files(dir,B,p);
