54bd56f77b3de951e4783da184fe056c718ebac5
valabregue <romain.valabregue@upmc.fr>
2018-04-04 16:24:50 +0200

    choose dicom ext as param

--- Tools/dicom/get_dir_recursif.m
+++ Tools/dicom/get_dir_recursif.m
@@ -1,4 +1,8 @@
-function F=get_dir_recursif(in_dir)
+function F=get_dir_recursif(in_dir,par)
+
+if ~exist('par','var'),par ='';end
+defpar.dic_ext='.dic';
+par = complet_struct(par,defpar);
 
 F=[];
   
@@ -11,13 +15,13 @@ for nbdir =1:size(in_dir,1)
   for i=1:size(DD,1)
 
     if ( DD(i).isdir )
-      F = [F;get_dir_recursif( fullfile(cur_dir,DD(i).name) )];
+      F = [F;get_dir_recursif( fullfile(cur_dir,DD(i).name) ,par)];
     else 
       [pathstr,filename,ext] = fileparts(DD(i).name);
       if ~isempty(ext)
 %	if  ~all(ext == '.log') & ~all(ext == '.txt')
     
-	if  strcmp(ext,'.dic') 
+	if  strcmp(ext,par.dic_ext) 
 	  F = [F;{fullfile(cur_dir,'/')}];
 	  break
 	end
