ed1cfb2db7eae900c66884119422c8a026c081b7
valabregue <romain.valabregue@upmc.fr>
2018-05-22 17:20:05 +0200

    default output is nii.gz

--- Wrappers/convert_mgz2nii.m
+++ Wrappers/convert_mgz2nii.m
@@ -17,7 +17,7 @@ job={};
 for k=1:length(fi)
   [p f e] = fileparts(fi{k});
   
-  fo{k} = fullfile(p,[f,'.nii']);
+  fo{k} = fullfile(p,[f,'.nii.gz']);
  
   if ~exist(fo{k})
 %    cmd = sprintf('source freesurfer5;export LD_LIBRARY_PATH=/usr/lib64:/lib64;source /usr/cenir/bincenir/freesurfer; mri_convert %s %s',fi{k},fo{k})
