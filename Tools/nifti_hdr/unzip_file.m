2b363d5586be959edae1b90cb651c527896b685b
valabregue <romain.valabregue@upmc.fr>
2018-06-10 11:03:22 +0200

    unzip et pas gunzip

---
+++ Tools/nifti_hdr/unzip_file.m
@@ -0,0 +1,42 @@
+function out = unzip_file(in,par)
+% UNZIP_VOLUME uses gunzip (linux) to unzip volumes if needed.
+% If the target file is not zipped (do not have .gz extension), skip it.
+
+
+% Ensure the inputs are cellstrings, to avoid dimensions problems
+in = cellstr(char(in));
+
+if ~exist('par','var'),par ='';end
+defpar.sge=0;
+defpar.jobname='zip';
+
+par = complet_struct(par,defpar);
+
+if isempty(in)
+    return
+end
+
+
+ind_to_remove=[];
+cmd = cell(size(in));
+for i=1:length(in)
+    
+    if ~isempty(in{i}) && ( strcmp(in{i}(end-1:end),'gz') ||  strcmp(in{i}(end-2:end),'zip'))
+        [pp na ex] = fileparts(in{i})
+        cmd{i} = sprintf('cd %s ; unzip -o  %s',pp,in{i});
+        out{i} = in{i}(1:end-3); %#ok<*AGROW>
+        
+    else
+        out{i} = in{i};
+        ind_to_remove(end+1)=i;
+    end
+    
+end
+
+cmd(ind_to_remove)=[];
+
+if ~isempty(cmd)
+    do_cmd_sge(cmd,par);
+end
+
+end % function
