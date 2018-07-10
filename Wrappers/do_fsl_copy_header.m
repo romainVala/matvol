f1e67cdba91502edac5c705366f75bebbbe41bb3
valabregue <romain.valabregue@upmc.fr>
2018-07-10 18:01:42 +0200

    add confirmation before changing the header

--- Wrappers/do_fsl_copy_header.m
+++ Wrappers/do_fsl_copy_header.m
@@ -1,12 +1,22 @@
-function fo = do_fsl_copy_header(fref,fchange)
-%function fo = do_fsl_copy_header(fref,fchange)
+function fo = do_fsl_copy_header(fchange,fref,par)
+%function fo = do_fsl_copy_header(fchange,fref)
 
 
 if ~exist('par'),par ='';end
 
 defpar.sge=0;
 defpar.d=1; % if true : do not copy image dimensions
+defpar.jobname='copy_hdr';
+defpar.ask = 1;
+
 par = complet_struct(par,defpar);
+
+
+fprintf('reference header will be \n %s\n',fref{1})
+if par.ask
+    d=  input('continue ?\n','s');
+end
+
 
 job={};
 for k=1:length(fref)
