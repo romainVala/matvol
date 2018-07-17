f04fb6c607124117de44e7ce840b321ec36c0aea
Benoît Béranger <benoit.beranger@icm-institute.org>
2018-07-17 15:29:23 +0200

    bugfix in @volume/removeGZ (applied to name + path)

--- objects/@volume/removeGZ.m
+++ objects/@volume/removeGZ.m
@@ -4,10 +4,20 @@ function removeGZ( volumeArray )
 
 for vol = 1 : numel(volumeArray)
     
-    for line = 1 : size(volumeArray(vol).name,1)
+    for lineNAME = 1 : size(volumeArray(vol).name,1)
         
-        if strcmp(volumeArray(vol).name(line,end-2:end),'.gz')
-            volumeArray(vol).name(:,end-2:end) = [];
+        idxNAME = strfind(volumeArray(vol).name(lineNAME,:),'.gz');
+        if ~isempty(idxNAME)
+            volumeArray(vol).name(:,idxNAME:end) = [];
+        end
+        
+    end % volume(i)
+    
+    for linePATH = 1 : size(volumeArray(vol).path,1)
+        
+        idxPATH = strfind(volumeArray(vol).path(linePATH,:),'.gz');
+        if ~isempty(idxPATH)
+            volumeArray(vol).path(:,idxPATH:end) = [];
         end
         
     end % volume(i)
