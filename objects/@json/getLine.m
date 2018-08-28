d622125593b5301654c5e33feab9227b15b19a49
Benoît Béranger <benoit.beranger@icm-institute.org>
2018-08-28 11:02:16 +0200

    @json/getLine enhancement

--- objects/@json/getLine.m
+++ objects/@json/getLine.m
@@ -1,4 +1,18 @@
-function out = getLine( volumeArray , regex )
+function out = getLine( volumeArray , regex, format )
+
+
+%% Check input arguments
+
+assert( ~isempty(regex ) && ischar(regex ),      'regex must be a non-empy char' )
+
+if nargin > 2
+    assert( ~isempty(format) && ischar(format), 'format must be a non-empy char' )
+else
+    format = '';
+end
+
+
+%% Fetch
 
 out = cell(size(volumeArray));
 
@@ -22,13 +36,14 @@ for vol = 1 : numel(volumeArray)
         % Fetch the line content
         start = regexp(content           , regex, 'once');
         stop  = regexp(content(start:end), ','  , 'once');
-        line = content(start:start+stop);
+        line = content(start:start+stop); % extract the value from the line
         token = regexp(line, ': (.*),','tokens');
         
         if ~isempty(token)
             
             res = token{1}{1};
             
+            % Remoce " at begining & end
             if strcmp(res(1),'"') && strcmp(res(end),'"')
                 out{vol} = res(2:end-1);
             else
@@ -45,10 +60,28 @@ for vol = 1 : numel(volumeArray)
     
 end % vol
 
-% Try to convert to numeric
-tmp = cellfun(@str2double, out);
-if ~all(isnan(tmp(:)))
-    out = tmp;
+
+%% Convert output type
+
+if ~isempty(format)
+    
+    switch lower(format)
+        case { 'double' , 'num' , 'numeric' }
+            out = cellfun(@str2double, out);
+        case { 'char' , 'string' , 'str' }
+            % pass
+        otherwise
+            error('unrecognized type : %s',format)
+    end
+    
+else
+    
+    % Try to convert to numeric
+    tmp = cellfun(@str2double, out);
+    if ~all(isnan(tmp(:)))
+        out = tmp;
+    end
+    
 end
 
 end % function
