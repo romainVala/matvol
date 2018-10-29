258a5fdc904819068bde396cbeceae1dea4f52b9
valabregue <romain.valabregue@upmc.fr>
2018-10-29 10:49:51 +0100

    change dict path dcmtk to lustre

--- Tools/dicom/send_dicom_prisma2.sh
+++ Tools/dicom/send_dicom_prisma2.sh
@@ -1,6 +1,6 @@
 #export DCMTK=/usr/cenir/src/dcmtk
 #export LD_LIBRARY_PATH=$DCMTK/lib
-export DCMDICTPATH=/export/data/opt/CENIR/dcmtk/dicom.dic
+export DCMDICTPATH=/network/lustre/iss01/cenir/software/irm/dcmtk/dicom.dic
 
 
 storescu -aet CENIR03  -aec IRM1_SEC 192.168.6.3 104 $*
--- Tools/dicom/send_dicom_verio.sh
+++ Tools/dicom/send_dicom_verio.sh
@@ -1,5 +1,5 @@
 #export DCMTK=/usr/cenir/src/dcmtk
 #export LD_LIBRARY_PATH=$DCMTK/lib
-export DCMDICTPATH=/export/data/opt/CENIR/dcmtk/dicom.dic
+export DCMDICTPATH=/network/lustre/iss01/cenir/software/irm/dcmtk/dicom.dic
 
 storescu -aet CENIR03  -aec IRM2_PRIM 192.168.6.4 104 $*
