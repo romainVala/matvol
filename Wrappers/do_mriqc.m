9db8b096cd2d0b9b7b45cb10e0b9b5822609e8c3
valabregue <romain.valabregue@upmc.fr>
2018-11-01 12:15:00 +0100

    option to chose subject id

--- Wrappers/do_mriqc.m
+++ Wrappers/do_mriqc.m
@@ -5,6 +5,8 @@ if ~exist('par'),par ='';end
 defpar.jobname = 'mriqc';
 defpar.outdir = '';
 defpar.sge=1;
+defpar.plabel='';
+% a mettre dans le do qsubcmd_prepend = sprintf(' module load mriqc\n source /network/lustre/iss01/cenir/software/irm/bin/python_path3.6\n '
 
 par = complet_struct(par,defpar);
 
@@ -18,20 +20,30 @@ if isempty(par.outdir)
     par.outdir = r_mkdir(pp,'mriqc_out');
     par.outdir=par.outdir{1}
 end
-    
+
 
 for nbbids=1:length(bids_dir)
     
     bdir = bids_dir{nbbids};
     
-    suj = gdir(bdir,'^sub');
-    
-    [pp sujname] = get_parent_path(suj);
-    
-    for kk=1:length(suj)
-        cmd{end+1} = sprintf('mriqc %s %s participant  --n_cpus 1  --ants-nthreads 1 ',bdir,par.outdir);
-        cmd{end} = sprintf('%s--ica --fft-spikes-detector --hmc-fsl --no-sub --verbose-reports --participant-label %s \n',...
-            cmd{end},sujname{kk}(5:end));
+    if ~isempty(par.plabel)
+        for kk=1:length(par.plabel)
+            cmd{end+1} = sprintf('mriqc %s %s participant  --n_cpus 1  --ants-nthreads 1 ',bdir,par.outdir);
+            cmd{end} = sprintf('%s--ica --fft-spikes-detector --hmc-fsl --no-sub --verbose-reports --participant-label %s \n',...
+                cmd{end},par.plabel{kk}(5:end));
+        end
+        
+    else
+        
+        suj = gdir(bdir,'^sub');
+        
+        [pp sujname] = get_parent_path(suj);
+        
+        for kk=1:length(suj)
+            cmd{end+1} = sprintf('mriqc %s %s participant  --n_cpus 1  --ants-nthreads 1 ',bdir,par.outdir);
+            cmd{end} = sprintf('%s--ica --fft-spikes-detector --hmc-fsl --no-sub --verbose-reports --participant-label %s \n',...
+                cmd{end},sujname{kk}(5:end));
+        end
     end
 end
 
