505520e19e1c94ea4baa17e1f241d3f6245c0b4f
valabregue <romain.valabregue@upmc.fr>
2018-11-30 08:46:16 +0100

    add B0 extract

--- Wrappers/do_mr_noise_remove.m
+++ Wrappers/do_mr_noise_remove.m
@@ -15,6 +15,10 @@ defpar.residual_prefix = 'noise_residual';
 defpar.sge=0;
 defpar.jobname = 'mr_noise';
 defpar.remove_gibs = 0;
+defpar.residual=0;
+defpar.extractB0 = 0;
+defpar.foB0 = '';
+defpar.B0_prefix='B0';
 
 defpar.walltime = '00:60:00';
 
@@ -32,20 +36,30 @@ end
 fonois = addprefixtofilenames(f,par.noise_prefix);
 fores = addprefixtofilenames(f,par.residual_prefix);
 for k=1:length(f)
-    
-    
+    %cmd = sprintf('dwidenoise %s - -noise %s |mrdegibbs - %s \n',f{k},fonois{k},fog{k});
+    cmd = sprintf('dwidenoise %s %s -noise %s\n',f{k},fo{k},fonois{k});
+    ffout = fo{k};
+    if par.residual
+        cmd = sprintf('%s mrcalc %s %s -subtract %s \n',cmd,f{k},fo{k},fores{k});
+    end
     if par.remove_gibs
-        cmd = sprintf('dwidenoise %s - -noise %s |mrdegibbs - %s \n',f{k},fonois{k},fog{k});
-        
-    else
-        cmd = sprintf('dwidenoise %s %s -noise %s\n',f{k},fo{k},fonois{k});
-        cmd = sprintf('%s mrcalc %s %s -subtract %s',cmd,f{k},fo{k},fores{k});
+        cmd = sprintf('%s mrdegibbs %s %s\n',cmd,fo{k},fog{k});
+        ffout = fog{k};
+    end
+    
+    if par.extractB0
+        if isempty(par.foB0)
+            foB0 = addprefixtofilenames(ffout,par.B0_prefix);
+        else
+            foB0 = par.foB0{k};
+        end
+        [dd ff] = get_parent_path(ffout);
+        cmd = sprintf('%s cd %s\n dwiextract %s -fslgrad bvecs bvals %s \n',...
+            cmd,dd,ff,foB0);
         
     end
     
     job{k} = cmd;
-    
-    
 end
 
 do_cmd_sge(job,par,jobappend)
