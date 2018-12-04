6e32b874e2bf1bb3b088d5219a383cae5c255399
valabregue <romain.valabregue@upmc.fr>
2018-12-04 15:07:14 +0100

    qualicati run casa_distro

---
+++ Wrappers/do_qualicati.m
@@ -0,0 +1,20 @@
+function  do_qualicati(fin,dout,par)
+
+if ~exist('par'),par ='';end
+
+defpar.jobname = 'qualicati';
+defpar.sge=1;
+
+par = complet_struct(par,defpar);
+
+
+cmd=repmat({},size(fin));
+
+for k=1:length(fin)
+    cmd{k} = sprintf('casa_distro run python /casa/src/qualicati/bug_fix/python/qualiCATI/Scripts/pipeline.py -f %s %s\n',...
+        dout{k},fin{k});
+end
+
+
+do_cmd_sge(cmd,par)
+
