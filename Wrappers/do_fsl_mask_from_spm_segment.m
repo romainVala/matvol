cffb16938115087ff58c3e4a397f993eac01dde8
valabregue <romain.valabregue@upmc.fr>
2018-12-05 12:06:26 +0100

    add fsl fillh and improve sge

--- Wrappers/do_fsl_mask_from_spm_segment.m
+++ Wrappers/do_fsl_mask_from_spm_segment.m
@@ -9,29 +9,45 @@ defpar.bin = 1;
 defpar.sge=0;
 defpar.jobname = 'fsl_mask';
 defpar.walltime = '00:10:00';
+defpar.brain_masked = 1;
 
 par = complet_struct(par,defpar);
 
 anat=get_parent_path(fanat);
 
-ff=get_subdir_regex_files(anat,'^[cp][123]',3);
+ff=get_subdir_regex_files(anat,'^[cp][123].*nii',3);
 fo=addsuffixtofilenames(anat,'/mask_brain');
 
 dosge=0;
 if par.sge, par.sge=-1;dosge=1;end
 
-[fm par.jobappend] = do_fsl_add(ff,fo,par);
-par
+[fm, par.jobappend] = do_fsl_add(ff,fo,par);
+[fm, par.jobappend] = do_fsl_fill(fm,par);
+
 
 %fm=get_subdir_regex_files(anat,'^mask_brain.nii',1); 
 
 par.type = {'erode','dilate','dilate','erode'};
 
 [fm2 par.jobappend]=mask_erode(fm,par);
-do_delete(fm,0)
+if par.sge
+    for k=1:length(fm)
+        par.jobappend{k} = sprintf('%s\n rm -f %s\n',par.jobappend{k},fm{k});
+    end
+else
+    do_delete(fm,0)
+end
 
 if dosge, par.sge=1;end
 
-fo = addprefixtofilenames(fanat,'brain_');
-job = do_fsl_mult(concat_cell(fm2,fanat),fo,par);
+if par.brain_masked
+    
+    fo = addprefixtofilenames(fanat,'brain_');
+    [~,job] = do_fsl_mult(concat_cell(fm2,fanat),fo,par);
+
+else
+    job = par.jobappend;
+    par = rmfield(par,'jobappend');
+    job = do_cmd_sge(job,par);
+end
 
