809dea498e73a766cd23177714169dd312af0797
valabregue <romain.valabregue@upmc.fr>
2018-12-11 16:49:27 +0100

    cleaner and robust to column versus ligne csv

--- Tools/stat_to_csv/read_res.m
+++ Tools/stat_to_csv/read_res.m
@@ -33,69 +33,43 @@ end% function
 %% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 function [varargout] = parse_csv(fin,par)
 
-ind_to_remove=[];
-if ischar(fin)
-    fin={fin};
-end
 
-for k=1:length(fin)
-    if k==2
-        keyboard
-    end
-    
-    [r h] = readtext(fin{k});
-    
-    % not working if some val are string
-    %    val = cell2mat(r(h.numberMask))';
-    %    hdr = r(h.stringMask)';
-    % so assume first line is hdr second is val and first collum only is a string
-    if ischar(r{2,1})
-        
+
+[r h] = readtext(fin);
+
+% not working if some val are string
+%    val = cell2mat(r(h.numberMask))';
+%    hdr = r(h.stringMask)';
+% so assume first line is hdr second is val and first collum only is a string
+if ischar(r{2,1})
+    if size(r,1)> size(r,2) %collumn
+        val = cell2mat(r(2:end,2))' ;
+        hdr = r(2:end,1)';
+    else %line
         val = cell2mat(r(2,2:end))' ;
         hdr = r(1,2:end)';
-    else
-        val = cell2mat(r(h.numberMask))';
-        hdr = r(h.stringMask)';
     end
-    
-    [hdr indh] = sort(hdr);
-    val = val(indh);
-    
-    if k==1
-        hdrref=hdr;
-    end
-    if length(hdr)~=length(hdrref)
-        fprintf('prb with %s\n',fin{k});
-        ind_to_remove(end+1) = k;
-        keyboard
-        continue
-    end
-    
-    if any(any(char(hdr)-char(hdrref)))
-        %error('not the same label')
-        fprintf('prb with %s\n',fin{k});
-        ind_to_remove(end+1) = k;
-        keyboard
-        continue
-    end
-    
-    vals(k,:) = val;
-    
+else
+    val = cell2mat(r(h.numberMask))';
+    hdr = r(h.stringMask)';
 end
 
-fin(ind_to_remove) = '';
+hdr = nettoie_dir(hdr);
+
+[hdr indh] = sort(hdr);
+val = val(indh);
+
 cout.suj = get_parent_path(fin);
-cout.ind_to_remove = ind_to_remove;
 
 for k=1:length(hdr)
-    cout = setfield(cout,hdr{k},vals(:,k));
-    
+    cout.(hdr{k}) = val(k); %vals(:,k);    
 end
 
 if nargout==1
     varargout{1} = cout;
 elseif nargout==2
     varargout{1} = hdrref;
-    varargout{2} = vals;
+    varargout{2} = val;
 end
+
 end
