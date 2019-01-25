40191368378aa9a04bca4f002b0047af44e0ffa9
valabregue <romain.valabregue@upmc.fr>
2019-01-25 14:24:29 +0100

    Revert "add a way to define exam from a list of dirs, with the regex equal to just_indir"
    This reverts commit e5a907e1c80cc129a4fe65e7e016a165f171400f.

--- objects/@exam/exam.m
+++ objects/@exam/exam.m
@@ -32,55 +32,45 @@ classdef exam < mvObject
                 AssertIsCharOrCellstr(indirs )
                 AssertIsCharOrCellstr(reg_ex)
                 
-                if strcmp(reg_ex,'just_indir')
-                    for idx = 1 : length(indirs)
-                        [pathstr,name, ~] = get_parent_path(indirs{idx});
-                        examArray(idx,1).name = name; %#ok<*AGROW>              % directory name
-                        examArray(idx,1).tag  = name;                           % initialization of the tag
-                        examArray(idx,1).path = fullfile(pathstr,name,filesep); % path of dirname
+                if ischar(indirs), indirs = {indirs};end
+                
+                idx_append = 0;
+                
+                for nb_in = 1:length(indirs)
+                    indir  = indirs{nb_in};
+                    reg_ex = char(reg_ex);
+                    
+                    % Is indir a real dir ?
+                    assert( exist(indir,'dir')==7, 'Dir does not exist : %s', indir )
+                    
+                    % Fetch dir list recursibley with regex
+                    if strcmp(reg_ex,'root')
+                        dirList={indir};
+                    else
+                        dirList = get_subdir_regex(indir, reg_ex, varargin{:});
                     end
-                else
                     
-                    if ischar(indirs), indirs = {indirs};end
-                    
-                    idx_append = 0;
-                    
-                    for nb_in = 1:length(indirs)
-                        indir  = indirs{nb_in};
-                        reg_ex = char(reg_ex);
-                        
-                        % Is indir a real dir ?
-                        assert( exist(indir,'dir')==7, 'Dir does not exist : %s', indir )
-                        
-                        % Fetch dir list recursibley with regex
-                        if strcmp(reg_ex,'root')
-                            dirList={indir};
-                        else
-                            dirList = get_subdir_regex(indir, reg_ex, varargin{:});
-                        end
-                        
-                        if numel(dirList) == 0
-                            warning('No dir found with regex [ %s ]\n in : %s', ...
-                                reg_ex, indir )
-                        elseif numel(dirList)==1 && isempty(dirList{1})
-                            warning('No dir selected graphically')
-                            examArray = exam.empty;
-                            return
-                        end
-                        
-                        % Create an array of @exam objects, corresponding to each dir in the list
-                        for idx = 1 : length(dirList)
-                            
-                            [pathstr,name, ~] = get_parent_path(dirList{idx});
-                            examArray(idx+idx_append,1).name = name; %#ok<*AGROW>              % directory name
-                            examArray(idx+idx_append,1).tag  = name;                           % initialization of the tag
-                            examArray(idx+idx_append,1).path = fullfile(pathstr,name,filesep); % path of dirname
-                            
-                            % NB : series field is an empty @serie object at the creation of the exam
-                            
-                        end
-                        idx_append = length(examArray);
+                    if numel(dirList) == 0
+                        warning('No dir found with regex [ %s ]\n in : %s', ...
+                            reg_ex, indir )
+                    elseif numel(dirList)==1 && isempty(dirList{1})
+                        warning('No dir selected graphically')
+                        examArray = exam.empty;
+                        return
                     end
+                    
+                    % Create an array of @exam objects, corresponding to each dir in the list
+                    for idx = 1 : length(dirList)
+                        
+                        [pathstr,name, ~] = get_parent_path(dirList{idx});
+                        examArray(idx+idx_append,1).name = name; %#ok<*AGROW>              % directory name
+                        examArray(idx+idx_append,1).tag  = name;                           % initialization of the tag
+                        examArray(idx+idx_append,1).path = fullfile(pathstr,name,filesep); % path of dirname
+                        
+                        % NB : series field is an empty @serie object at the creation of the exam
+                        
+                    end
+                    idx_append = length(examArray);
                 end
             end
             
