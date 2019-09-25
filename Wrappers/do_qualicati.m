99b4ae7103bf7576823f26e35d0797b47ca673b5
valabregue <romain.valabregue@upmc.fr>
2019-09-25 10:19:51 +0200

    add option

--- Wrappers/do_qualicati.m
+++ Wrappers/do_qualicati.m
@@ -3,6 +3,8 @@ function  do_qualicati(fin,dout,par)
 if ~exist('par'),par ='';end
 
 defpar.jobname = 'qualicati';
+defpar.repo = '/network/lustre/iss01/cenir/software/irm/brainvisa_src/casa_distro_new';
+defpar.run_script = '/casa/src/qualicati/bug_fix/python/qualiCATI/Scripts/pipeline.py';
 defpar.sge=1;
 
 par = complet_struct(par,defpar);
@@ -11,8 +13,8 @@ par = complet_struct(par,defpar);
 cmd=repmat({},size(fin));
 
 for k=1:length(fin)
-    cmd{k} = sprintf('casa_distro run python /casa/src/qualicati/bug_fix/python/qualiCATI/Scripts/pipeline.py -f %s %s\n',...
-        dout{k},fin{k});
+    cmd{k} = sprintf('casa_distro -r %s run python %s -f %s %s\n',...
+        par.repo,par.run_script,dout{k},fin{k});
 end
 
 
