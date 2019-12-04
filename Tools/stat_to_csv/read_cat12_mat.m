e4910acb7dd814a5dd5210c6c3906bbb58c0ccab
valabregue <romain.valabregue@upmc.fr>
2019-12-04 10:53:03 +0100

    imporved

--- Tools/stat_to_csv/read_cat12_mat.m
+++ Tools/stat_to_csv/read_cat12_mat.m
@@ -1,4 +1,4 @@
-function c = read_cat12_mat(fin)
+function [cout, dir_missing] = read_cat12_mat(fin)
 %fin list of
 
 
@@ -11,14 +11,16 @@ if isfield(S,'qualitymeasures')
     
     cfield = {'meanB','meanC','meanG','meanW','stdB','stdC','stdG','stdW','contrast','contrastr',...
         'NCR','ICR','qr_noise','qr_bias','IQR','vol_C','vol_G','vol_W','vol_WH','sr_TIV','SQR'}
-    vinit = ones(size(fin))*NaN; for kk =1:length(cfield), c.(cfield{kk})=vinit;end
+    vinit = ones(size(fin))*NaN; for kk =1:length(cfield), cout.(cfield{kk})=vinit;end
 end
+dir_missing={};
 
 for k=1:length(fin)
     try 
         load(fin{k})
     catch 
         fprintf('corupt %s\n',fin{k})
+        dir_missing(end+1) = fin{k};
         continue
         
     end
@@ -26,21 +28,21 @@ for k=1:length(fin)
     found=0;
     
     if isfield(S,'qualitymeasures')
-        c.suj{k} = fin{k};
-        c.meanB(k) = S.qualitymeasures.tissue_mn(1);c.meanC(k) = S.qualitymeasures.tissue_mn(2);
-        c.meanG(k) = S.qualitymeasures.tissue_mn(3);c.meanW(k) = S.qualitymeasures.tissue_mn(4);
-        c.stdB(k) = S.qualitymeasures.tissue_std(1);c.stdC(k) = S.qualitymeasures.tissue_std(2);
-        c.stdG(k) = S.qualitymeasures.tissue_std(3);c.stdW(k) = S.qualitymeasures.tissue_std(4);
-        c.contrast(k) = S.qualitymeasures.contrast; c.contrastr(k) = S.qualitymeasures.contrastr;
-        c.NCR(k) = S.qualitymeasures.NCR;c.ICR(k) = S.qualitymeasures.ICR;
+        cout.suj{k} = fin{k};
+        cout.meanB(k) = S.qualitymeasures.tissue_mn(1);cout.meanC(k) = S.qualitymeasures.tissue_mn(2);
+        cout.meanG(k) = S.qualitymeasures.tissue_mn(3);cout.meanW(k) = S.qualitymeasures.tissue_mn(4);
+        cout.stdB(k) = S.qualitymeasures.tissue_std(1);cout.stdC(k) = S.qualitymeasures.tissue_std(2);
+        cout.stdG(k) = S.qualitymeasures.tissue_std(3);cout.stdW(k) = S.qualitymeasures.tissue_std(4);
+        cout.contrast(k) = S.qualitymeasures.contrast; cout.contrastr(k) = S.qualitymeasures.contrastr;
+        cout.NCR(k) = S.qualitymeasures.NCR;cout.ICR(k) = S.qualitymeasures.ICR;
         
-        c.qr_noise(k) = mark2rps(S.qualityratings.NCR);
-        c.qr_bias(k)  = mark2rps(S.qualityratings.ICR);
-        c.IQR(k)      = mark2rps(S.qualityratings.IQR);
+        cout.qr_noise(k) = mark2rps(S.qualityratings.NCR);
+        cout.qr_bias(k)  = mark2rps(S.qualityratings.ICR);
+        cout.IQR(k)      = mark2rps(S.qualityratings.IQR);
         
-        c.vol_C(k) = S.subjectmeasures.vol_abs_CGW(1);c.vol_G(k) = S.subjectmeasures.vol_abs_CGW(2);
-        c.vol_W(k) = S.subjectmeasures.vol_abs_CGW(3);c.vol_WH(k) = S.subjectmeasures.vol_abs_CGW(4);
-        c.sr_TIV(k) = S.subjectratings.vol_TIV; c.SQR(k) = S.subjectratings.SQR;
+        cout.vol_C(k) = S.subjectmeasures.vol_abs_CGW(1);cout.vol_G(k) = S.subjectmeasures.vol_abs_CGW(2);
+        cout.vol_W(k) = S.subjectmeasures.vol_abs_CGW(3);cout.vol_WH(k) = S.subjectmeasures.vol_abs_CGW(4);
+        cout.sr_TIV(k) = S.subjectratings.vol_TIV; cout.SQR(k) = S.subjectratings.SQR;
         
         found=1;
     end
@@ -48,7 +50,7 @@ for k=1:length(fin)
         
         colname = S.lpba40.names; colname = strrep(colname,'+','_');
         for kk=1:length(colname)
-            c.([ 'ba40_' colname{kk} '_gm'])(k) = S.lpba40.data.Vgm(kk);
+            cout.([ 'ba40_' colname{kk} '_gm'])(k) = S.lpba40.data.Vgm(kk);
         end
         
         found=1;
@@ -58,16 +60,17 @@ for k=1:length(fin)
         
         colname = S.hammers.names;  colname = strrep(colname,'+','_');
         for kk=1:length(colname);
-            c.([ 'ham_' colname{kk} '_gm'])(k) = S.hammers.data.Vgm(kk);
-            c.([ 'ham_' colname{kk} '_wm'])(k) = S.hammers.data.Vwm(kk);
-            c.([ 'ham_' colname{kk} '_csf'])(k) = S.hammers.data.Vcsf(kk);
+            cout.([ 'ham_' colname{kk} '_gm'])(k) = S.hammers.data.Vgm(kk);
+            cout.([ 'ham_' colname{kk} '_wm'])(k) = S.hammers.data.Vwm(kk);
+            cout.([ 'ham_' colname{kk} '_csf'])(k) = S.hammers.data.Vcsf(kk);
         end
         
         found=1;
     end
     
     if ~found
-        fprintf('Suject %s failled\n',fin{k});
+        fprintf('Suject %s failled (no field) \n',fin{k});
+        dir_missing(end+1) = fin{k};
     end
     
     
