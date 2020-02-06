8f4aba83d9e74ab9dba43e72a2f71758b2dc6e15
Benoît Béranger <benoit.beranger@icm-institute.org>
2020-02-06 21:20:16 +0100

    cat12 segmentation : - much better help, maybe to much, but precise - new options to write volumes - new options (bias correction strength, affine preprocessing
    todo : add objects when done

--- SPM/preproc/job_do_segmentCAT12.m
+++ SPM/preproc/job_do_segmentCAT12.m
@@ -1,17 +1,20 @@
-function  jobs = job_do_segmentCAT12(img,par)
+function jobs = job_do_segmentCAT12(img,par)
+% JOB_DO_SEGMENTCAT12
 %
-% INPUT : img can be 'char' of volume(file), single-level 'cellstr' of volume(file), '@volume' array
+% SYNTAX
+% JOB_DO_SEGMENTCAT12(img,par)
 %
-% for spm12 segment, if img{1} has several line then it is a multichannel
+% INPUT
+% - img : can be 'char' of volume(file), single-level 'cellstr' of volume(file), '@volume' array
+% - par : classic matvol parameter structure
 %
-% 'par' allow you to specify which output to save  defaults are
-%   par.GM   = [0 0 1 0]; % warped_space_Unmodulated(wp*) / warped_space_modulated(mwp*) / native_space(p*) / native_space_dartel_import(rp*)
-%   par.WM   = [0 0 1 0];
-%   par.CSF  = [0 0 1 0];
-%   par.bias = [0 1]; % bias field / bias corrected image
-%   par.warp = [1 1]; % warp field native->template / warp field native<-template
+% IMPORTANT NOTE
+% Check the "defpar" section below for tuning your job
 %
-% See also get_subdir_regex get_subdir_regex_files exam exam.AddSerie exam.addVolume
+%
+% See also get_subdir_regex get_subdir_regex_files exam exam.addSerie exam.addVolume
+
+if nargin==0, help(mfilename), return, end
 
 
 %% Check CAT12 toolbox install
@@ -44,53 +47,105 @@ end
 
 %% defpar
 
-% Retrocompatibility for SPM:Spatial:Segment options
-defpar.GM        = [0 0 1 0]; % warped_space_Unmodulated(wp*) / warped_space_modulated(mwp*) / native_space(p*) / native_space_dartel_import(rp*)
-defpar.WM        = [0 0 1 0];
-defpar.CSF       = [0 0 1 0];
-defpar.bias      = [1 0 0] ;  % native (ms*) / normalize (wms*) / dartel (rms*)     This will save the bias field corrected T1 
-defpar.label     = [0 0 0] ;  % native (p0*) / normalize (wp0*) / dartel (rp0*)    This will create a label map
-defpar.TPMC      = [0 0 0] ;  % native  (p4*, p5*, p6*) / normalize (w*) / dartel (r*)     This wil create other probalities map (p4 p5 p6)
-defpar.warp      = [1 1];     % warp field native->template / warp field native<-template
+%- cat12 segmentation options ---------------------------------------------
 
-defpar.jacobian  = 1;         % write jacobian determinant in normalize space
-defpar.doROI     = 0;
+defpar.subfolder = 0;         % 0 means "do not write in subfolder"
+
+% Strength of the SPM inhomogeneity (bias) correction that simultaneously controls the SPM biasreg and biasfwhm parameter.
+% Modify this value only if you experience any problems!
+% Use smaller values (>0) for slighter corrections (e.g. in synthetic contrasts without visible bias) and higher values (<=1) for stronger corrections (e.g. in 7 Tesla data).
+% Bias correction is further controlled by the Affine Preprocessing (APP).
+%  eps -> ultralight
+% 0.25 -> light
+% 0.50 -> medium (CAT12 default)
+% 0.75 -> strong
+% 1.00 -> heavy
+defpar.biasstr   = 0.5;
+
+% Parameter to control the accuracy of SPM preprocessing functions. In most images the standard accuracy is good enough for the initialization in CAT.
+% However, some images with servere (local) inhomogeneities or atypical anatomy may benefit by additional iterations and higher resolution.
+%  eps -> ltra low  (superfast)
+% 0.25 -> low       (fast)
+% 0.50 -> average   (default)
+% 0.75 -> high      (slow)
+% 1.00 -> ulta high (very slow)
+defpar.accstr    = 0.5;
+
+% warped_space_modulated    (mwp**)  0 -> No
+%                                    1 -> Affine + non-linear (SPM12 default)
+%                                    2 -> Non-Linear only
+%
+% native_space_dartel_import (rp**)  0 -> No
+%                                    1 -> Rigid (SPM12 default)
+%                                    2 -> Affine
+%                                    3 -> Both
+%
+% Values can be :               0,1                                 / 0,1,2                              / 0,1                    / 0,1,2,3
+defpar.GM        = [0 0 1 0]; % warped_space_Unmodulated (wp1*)     / warped_space_modulated (mwp1*)     / native_space (p1*)     / native_space_dartel_import (rp1*)
+defpar.WM        = [0 0 1 0]; %                          (wp2*)     /                        (mwp2*)     /              (p2*)     /                            (rp2*)
+defpar.CSF       = [0 0 1 0]; %                          (wp3*)     /                        (mwp3*)     /              (p3*)     /                            (rp3*)
+defpar.TPMC      = [0 0 1 0]; %                          (wp[456]*) /                        (mwp[456]*) /              (p[456]*) /                            (rp[456]*)   This will create other probalities map (p4 p5 p6)
+
+% dartel (rp0*)(rp[456]*)(rms*)      0 -> No
+%                                    1 -> Rigid (SPM12 default)
+%                                    2 -> Affine
+%                                    3 -> Both
+%
+% Values can be :               0,1              / 0,1              / 0,1,2,3
+defpar.label     = [0 0 0] ;  % native (p0*)     / normalize (wp0*) / dartel (rp0*)       This will create a label map : p0 = (1 x p1) + (3 x p2) + (1 x p3)
+defpar.bias      = [1 1 0] ;  % native (ms*)     / normalize (wms*) / dartel (rms*)       This will save the bias field corrected  + SANLM T1
+
+defpar.warp      = [1 1];     % Warp fields  : native->template (y_*) / native<-template (iy_*)
+
+% Surface
+% 0  -> No
+% 1  -> lh + rh
+% 2  -> lh + rh + cerebellum
+% 5  -> lh + rh (fast, no registration)
+% 6  -> lh + rh + cerebellum (fast, no registration)
+% 7  -> lh + rh (fast registration)
+% 8  -> lh + rh + cerebellum (fast registration)
+% 9  -> Thickness estimation (for ROI analysis only)
+% 12 -> Full
 defpar.doSurface = 0;
-defpar.subfolder = 0; % all results in the same subfolder
 
+% Atlas
+% - neuromorphics
+% - ipba40
+% - cobra
+% - hammers
+% - ibsr
+% - aal
+% - mori
+% - anatomy
+defpar.doROI     = 0;         % Will compute the volume in each atlas region
+
+defpar.jacobian  = 1;         % Write jacobian determinant in normalize space
+
+
+%--------------------------------------------------------------------------
+
+% matvol classics
+defpar.run          = 1;
+defpar.display      = 0;
+defpar.redo         = 0;
+defpar.sge          = 0;
 defpar.auto_add_obj = 1;
 
-defpar.run     = 1;
-defpar.display = 0;
-defpar.redo    = 0;
-defpar.sge     = 0;
-
-defpar.jobname  = 'spm_segmentCAT';
-defpar.walltime = '02:00:00';
+% cluster
+defpar.jobname  = 'spm_segmentCAT12';
+defpar.walltime = '04:00:00';
 
 par = complet_struct(par,defpar);
 
-if any(par.TPMC),    expert_mode=2; else expert_mode=1; end
-defpar.cmd_prepend = sprintf('global cat; cat_defaults; cat.extopts.subfolders=%d; cat.extopts.expertgui=%d;clear defaults; spm_jobman(''initcfg'');',...
-    par.subfolder,expert_mode);
-defpar.matlab_opt = ' -nodesktop ';
-
+% subfolder trick : the line bellow will be executed before the spm_jobman(), especially useful for cluster
+defpar.cmd_prepend = sprintf('global cat; cat_defaults; cat.extopts.subfolders=%d;',par.subfolder);
+defpar.matlab_opt  = ' -nodesktop ';
 
 par = complet_struct(par,defpar);
-
-% Security
-if par.sge
-    par.auto_add_obj = 0;
-end
 
 
 %% Prepare job generation
-
-%to make expert mode active if not done
-global cat;cat_defaults;
-if cat.extopts.expertgui==0
-    eval(par.cmd_prepend)
-end
 
 % unzip volumes if required
 if obj
@@ -117,63 +172,73 @@ for nbsuj = 1:length(img)
         fprintf('[%s]: skiping subj %d because %s exist \n',mfilename,nbsuj,of{1});
     end
     
-    %spm_dir=spm('Dir'); %fileparts(which ('spm'));
     jobs{nbsuj}.spm.tools.cat.estwrite.data = cellstr(img{nbsuj}); %#ok<*AGROW>
     jobs{nbsuj}.spm.tools.cat.estwrite.nproc = 0;
-    %     jobs{nbsuj}.spm.tools.cat.estwrite.opts.tpm = {fullfile(spm_dir,'tpm','TPM.nii,1')};
-    %     jobs{nbsuj}.spm.tools.cat.estwrite.opts.affreg = 'mni';
-    %     jobs{nbsuj}.spm.tools.cat.estwrite.opts.biasstr = 0.5;
-    %     jobs{nbsuj}.spm.tools.cat.estwrite.extopts.APP = 1070;
-    %     jobs{nbsuj}.spm.tools.cat.estwrite.extopts.LASstr = 0.5;
-    %     jobs{nbsuj}.spm.tools.cat.estwrite.extopts.gcutstr = 0.5;
-    %     jobs{nbsuj}.spm.tools.cat.estwrite.extopts.cleanupstr = 0.5;
-    %     jobs{nbsuj}.spm.tools.cat.estwrite.extopts.registration.darteltpm = {fullfile(spm_dir,'toolbox','cat12','templates_1.50mm','Template_1_IXI555_MNI152.nii')};
-    %     jobs{nbsuj}.spm.tools.cat.estwrite.extopts.registration.shootingtpm = {fullfile(spm_dir,'toolbox','cat12','templates_1.50mm','Template_0_IXI555_MNI152_GS.nii')};
-    %     jobs{nbsuj}.spm.tools.cat.estwrite.extopts.registration.regstr = 0;
-    %     jobs{nbsuj}.spm.tools.cat.estwrite.extopts.vox = 1.5;
     
-    %defpar.GM   = [0 0 1 0]; % Unmodulated / modulated / native_space / dartel import
+    jobs{nbsuj}.spm.tools.cat.estwrite.opts.bias.biasstr = par.biasstr;
+    jobs{nbsuj}.spm.tools.cat.estwrite.opts.acc.accstr   = par.accstr;
     
     % ROI
-    if par.doROI==0
+    if par.doROI
+        jobs{nbsuj}.spm.tools.cat.estwrite.output.ROImenu.atlases.neuromorphometrics = 1;
+        jobs{nbsuj}.spm.tools.cat.estwrite.output.ROImenu.atlases.lpba40             = 1;
+        jobs{nbsuj}.spm.tools.cat.estwrite.output.ROImenu.atlases.cobra              = 1;
+        jobs{nbsuj}.spm.tools.cat.estwrite.output.ROImenu.atlases.hammers            = 1;
+        jobs{nbsuj}.spm.tools.cat.estwrite.output.ROImenu.atlases.ibsr               = 1;
+        jobs{nbsuj}.spm.tools.cat.estwrite.output.ROImenu.atlases.aal                = 1;
+        jobs{nbsuj}.spm.tools.cat.estwrite.output.ROImenu.atlases.mori               = 1;
+        jobs{nbsuj}.spm.tools.cat.estwrite.output.ROImenu.atlases.anatomy            = 1;
+    else
         jobs{nbsuj}.spm.tools.cat.estwrite.output.ROImenu.noROI = struct([]);
     end %else take the default
     
     % Surface
     jobs{nbsuj}.spm.tools.cat.estwrite.output.surface = par.doSurface;
     
-    % TPM
+    %----------------------------------------------------------------------
+    %- Tissue Probability Maps (TMP)
+    
+    % GM
     jobs{nbsuj}.spm.tools.cat.estwrite.output.GM.warped = par.GM(1);
     jobs{nbsuj}.spm.tools.cat.estwrite.output.GM.mod    = par.GM(2);
     jobs{nbsuj}.spm.tools.cat.estwrite.output.GM.native = par.GM(3);
-    if par.GM(4), jobs{nbsuj}.spm.tools.cat.estwrite.output.GM.dartel = 2; end % 0==none, 1==rigid, 2==affine, 3==rigid+affine
+    jobs{nbsuj}.spm.tools.cat.estwrite.output.GM.dartel = par.GM(4);
     
+    % WM
     jobs{nbsuj}.spm.tools.cat.estwrite.output.WM.warped = par.WM(1);
     jobs{nbsuj}.spm.tools.cat.estwrite.output.WM.mod    = par.WM(2);
     jobs{nbsuj}.spm.tools.cat.estwrite.output.WM.native = par.WM(3);
-    if par.WM(4), jobs{nbsuj}.spm.tools.cat.estwrite.output.WM.dartel = 2; end % 0==none, 1==rigid, 2==affine, 3==rigid+affine
+    jobs{nbsuj}.spm.tools.cat.estwrite.output.WM.dartel = par.WM(4);
     
+    % CSF
     jobs{nbsuj}.spm.tools.cat.estwrite.output.CSF.warped = par.CSF(1);
     jobs{nbsuj}.spm.tools.cat.estwrite.output.CSF.mod    = par.CSF(2);
     jobs{nbsuj}.spm.tools.cat.estwrite.output.CSF.native = par.CSF(3);
-    if par.CSF(4), jobs{nbsuj}.spm.tools.cat.estwrite.output.CSF.dartel = 2; end % 0==none, 1==rigid, 2==affine, 3==rigid+affine
+    jobs{nbsuj}.spm.tools.cat.estwrite.output.CSF.dartel = par.CSF(4);
+    
+    % TMPC
+    jobs{nbsuj}.spm.tools.cat.estwrite.output.TPMC.native = par.TPMC(1);
+    jobs{nbsuj}.spm.tools.cat.estwrite.output.TPMC.mod    = par.TPMC(2);
+    jobs{nbsuj}.spm.tools.cat.estwrite.output.TPMC.warped = par.TPMC(3);
+    jobs{nbsuj}.spm.tools.cat.estwrite.output.TPMC.dartel = par.TPMC(4);
+    
+    %----------------------------------------------------------------------
     
     % Bias field, using SANML for denoising
     jobs{nbsuj}.spm.tools.cat.estwrite.output.bias.native = par.bias(1);
     jobs{nbsuj}.spm.tools.cat.estwrite.output.bias.warped = par.bias(2);
-    jobs{nbsuj}.spm.tools.cat.estwrite.output.bias.dartel = par.bias(3)*2;
-
+    jobs{nbsuj}.spm.tools.cat.estwrite.output.bias.dartel = par.bias(3);
+    jobs{nbsuj}.spm.tools.cat.estwrite.output.las .native = par.bias(1);
+    jobs{nbsuj}.spm.tools.cat.estwrite.output.las .warped = par.bias(2);
+    jobs{nbsuj}.spm.tools.cat.estwrite.output.las .dartel = par.bias(3);
+    
+    % Labels
     jobs{nbsuj}.spm.tools.cat.estwrite.output.label.native = par.label(1);
     jobs{nbsuj}.spm.tools.cat.estwrite.output.label.warped = par.label(2);
-    jobs{nbsuj}.spm.tools.cat.estwrite.output.label.dartel = par.label(3)*2;
+    jobs{nbsuj}.spm.tools.cat.estwrite.output.label.dartel = par.label(3);
     
-    if any(par.TPMC)
-        jobs{nbsuj}.spm.tools.cat.estwrite.output.TPMC.native = par.TPMC(1);
-        jobs{nbsuj}.spm.tools.cat.estwrite.output.TPMC.warped = par.TPMC(2);
-        jobs{nbsuj}.spm.tools.cat.estwrite.output.TPMC.dartel = par.TPMC(3)*2;
-    end
-
-    jobs{nbsuj}.spm.tools.cat.estwrite.output.jacobian.warped = par.jacobian;
+    % Jacobian
+    jobs{nbsuj}.spm.tools.cat.estwrite.output.jacobianwarped = par.jacobian;
     
     % Warp fields : y_ & iy_
     jobs{nbsuj}.spm.tools.cat.estwrite.output.warps = par.warp;
