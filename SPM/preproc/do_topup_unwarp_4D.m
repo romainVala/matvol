921b97207c3d11619bb5f6aef7c3eab9f3354c5d
Benoît Béranger <benoit.beranger@icm-institute.org>
2020-03-04 15:15:36 +0100

    do_topup_unwarp_4D : fix compatibility with workflow

--- SPM/preproc/do_topup_unwarp_4D.m
+++ SPM/preproc/do_topup_unwarp_4D.m
@@ -110,7 +110,7 @@ for subj=1:nrSubject
         
         % Is the orientation of all runs coherent ?
         if run>1
-            if compare_orientation(fmean(1),runList(run)) == 0
+            if ~sge && compare_orientation(fmean(1),runList(run)) == 0
                 warning('[%s]: WARNING reslicing mean image %s \n', mfilename, mean_files_cellstr{1});
                 [ resliced_mean, job_subj ]= do_fsl_reslice( mean_files_cellstr(1),fmean(1),'', job_subj);
                 mean_files_cellstr(1) = resliced_mean;
