3025cb08c044d1a7af56983fd87d015453386613
AudreyThirel <thirelaudrey@gmail.com>
2020-07-15 15:58:17 +0200

    Job AFNI->SPM

--- SPM/preproc/job_rp_afni2spm.m
+++ SPM/preproc/job_rp_afni2spm.m
@@ -1,35 +1,35 @@
 function job_rp_afni2spm(dfile, output_dir)
-%
-% INPUTS : 
-% dfile = inputs directory
-% output_dir = outputs directory 
+% Convert the data provided by AFNI (dfile.*1D files) to the same format as SPM data
+% Converted data will be saved in a file called rp_spm.txt at the chosen
+% location (output_dir)
 
+% INPUTS :
+% dfile      = inputs  directory, must be cellstr
+% output_dir = outputs directory, must be cellstr
 
-for i = 1:length(dfile)
-    rp_afni = load(dfile(i).name);
+N = length(dfile);
+
+for i = 1:N
     
-% Translation
-Tx = rp_afni(:,4);
-Ty = rp_afni(:,5);
-Tz = rp_afni(:,6);
-
-% Rotation : deg -> rad
-Rx = rp_afni(:,1)*pi/180;
-Ry = rp_afni(:,2)*pi/180;
-Rz = rp_afni(:,3)*pi/180;
-
-Datas=[Tx, Ty, Tz, Rx, Ry, Rz];
-
-% file .txt
-filename = fullfile(output_dir,sprintf('rp_spm.txt'));
+    % Load text file
+    rp_afni = load(dfile{i});
     
-save(filename,'Datas','-ascii','-double','-tabs')
-
+    % Translation : keep mm
+    Tx = rp_afni(:,4);
+    Ty = rp_afni(:,5);
+    Tz = rp_afni(:,6);
+    
+    % Rotation : deg -> rad
+    Rx = rp_afni(:,1)*pi/180;
+    Ry = rp_afni(:,2)*pi/180;
+    Rz = rp_afni(:,3)*pi/180;
+    
+    Datas=[Tx, Ty, Tz, Rx, Ry, Rz]; %#ok<NASGU>
+    
+    % file .txt
+    filename = fullfile(output_dir{i},'rp_spm.txt');
+    save(filename,'Datas','-ascii','-double','-tabs')
+    fprintf('saving file (%d,%d) : %s \n', i, N, filename)
+    
 end
-
-
-
-
-
-
 
