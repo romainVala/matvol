d2c24f3ef00d3d32ac13ae016a7e06bb76783678
askrzatek <skrzatek.a@gmail.com>
2020-07-17 12:13:51 +0200

    prefix for warped anatomical image change - ^w replacing ^wms

--- SPM/job_spm_single_results_display.m
+++ SPM/job_spm_single_results_display.m
@@ -6,8 +6,8 @@ function job_spm_single_results_display (fsub, Coordlist, par)
 % fsub = {'/network/lustre/iss01/cenir/analyse/irm/users/anna.skrzatek/nifti/2019_02_27_REMINARY_HM_005_V2/model_meica/'} % EXAMPLE for one subject
 %
 %   COORDLIST : structure variable defining ROI's names and coordinates to display
-% Coordlist.values = cat(2,[0.0; 0.0; 0.0], [34.0; -24.0; 68.0], [-34.0; -24.0; 68.0], [-4.0; -48.0; -24.0], [0.0; -24.0; 10.0]); % EXAMPLE
-% Coordlist.names = {'centre'; 'RIGHT SM'; 'LEFT SM'; 'CEREBELLUM'; 'MOTOR BI'}; % EXAMPLE
+% Coordlist.values = cat(2,[0.0; 0.0; 0.0], [34.0; -24.0; 68.0], [-34.0; -24.0; 68.0], [-4.0; -48.0; -24.0], [0.0; -24.0; 10.0], [-1.0; -9.0; 58.0], [-36.0; -25.0; 57.0], [3.0; -68.0; -12.0]); % EXAMPLE
+% Coordlist.names = {'centre'; 'RIGHT SM'; 'LEFT SM'; 'CEREBELLUM'; 'MOTOR BI'; 'SMA_L'; 'M1_L'; 'CB_V_R';}; % EXAMPLE
 %
 %   PAR : strucutre containing all variable parameters
 % .RUN           : (0/1) prepares the commands with (1) or without (0) executing them immediately
@@ -18,6 +18,9 @@ function job_spm_single_results_display (fsub, Coordlist, par)
 % .SUBDIR        : char name of the working directory (wd) containing the SPM.mat file
 % .OUTPUT_DIR    : char name of the output directory where we will save created figures : must be or will be created INSIDE the wd
 % .CONNAME       : char name of the contrast we want to create figures for (only 1 at a time) - case insensitive
+% .FIXSCALE
+% .MINSCALE
+% .MAXSCALE
 %
 % + CLASSIC MATLAB PARAMETERS FOR SPM RESULTS SECTION LIKE:
 % .CONTRASTS     : int number of the contrast in case conname unknown or not found in SPM.mat
@@ -35,7 +38,7 @@ function job_spm_single_results_display (fsub, Coordlist, par)
     defpar.display = 0; % could be 1 in this case (to test)
     defpar.redo = 0;
     defpar.anat_dir_reg = 'S\d{2}_t1mpr_S256_0_8iso_p2$';
-    defpar.anat_file_reg = '^wms_S\d{2}.*p2.nii';
+    defpar.anat_file_reg = '^wp0.*p2.nii';
     defpar.subdir = 'model_meica';
     defpar.output_dir = 'figures_meica';
     defpar.conname = 'F-all'; % default corresponding to defpar.contrasts = 1 if user doesn't give any name of the contrast
@@ -50,6 +53,10 @@ function job_spm_single_results_display (fsub, Coordlist, par)
     defpar.units = 1;
     defpar.print = 0;
     defpar.write.none = 0;
+    
+    defpar.fixscale = 0; % default automatic scale if you want to apply min/maxscale params fixscale must be 1
+    defpar.minscale = 0;
+    defpar.maxscale = 30;
     
     par = complet_struct(par, defpar);
     
@@ -115,7 +122,8 @@ function job_spm_single_results_display (fsub, Coordlist, par)
         
               
         spm_sections(xSPM,hReg,t1);
-
+        
+        
             %% Go to the position : aka ROI choice
          for iROI = 1 : length(Coordlist.values)
 
@@ -137,6 +145,15 @@ function job_spm_single_results_display (fsub, Coordlist, par)
              graphName = char(addsuffixtofilenames(addsuffixtofilenames(Coordlist.names(iROI),'_'),xSPM.title)); % choose the name according to the ROI -> Contrast name not needed if we have folder architecture
                 
              global st
+                
+             %% step to control the activation scale limits %% maybe I could move it outside the ROI loop - to test
+                if par.fixscale == 1
+                    st.vols{1}.blobs{1}.min = par.minscale;
+                    st.vols{1}.blobs{1}.max = par.maxscale;
+                    spm_orthviews('redraw')
+                end
+             %% step to change cursor position
+                
                 X  = frame2im(getframe(st.fig));
                 sz = size(X);
                 sz = [sz(1) sz(1) sz(2) sz(2)];
@@ -147,6 +164,8 @@ function job_spm_single_results_display (fsub, Coordlist, par)
                 sz = ([1-a(2)-a(4),1-a(2),a(1),a(1)+a(3)] .* (sz-1)) + 1;
                 sz = round(sz);
                 X  = X(sz(1):sz(2),sz(3):sz(4),:);
+                
+
                 
                 output_dir = char(addsuffixtofilenames(fspm, par.output_dir));
                 con_output_dir = char(addsuffixtofilenames(addsuffixtofilenames(output_dir,'/'),par.conname));
