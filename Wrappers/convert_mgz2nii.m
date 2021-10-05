d5aea7f2e642a5e0866caca06daf243602fc1c7a
Salim ouarab <ouarabsalim8@gmail.com>
2021-10-05 14:44:42 +0200

    add param to use it in cluster

--- Wrappers/convert_mgz2nii.m
+++ Wrappers/convert_mgz2nii.m
@@ -1,9 +1,17 @@
-function fo = convert_mgz2nii(fi,par)
+function fo = convert_mgz2nii(fi,params)
 
 
 if ~exist('par')
     par='';
 end
+
+def_par.version_path='module load FreeSurfer/7.1.1';
+def_par.jobname = 'mgz2nii';
+def_par.sge = 0;
+
+par = complet_struct(params,def_par);
+
+
 
 
 if ~exist('fi')
@@ -21,7 +29,8 @@ for k=1:length(fi)
  
   if ~exist(fo{k})
 %    cmd = sprintf('source freesurfer5;export LD_LIBRARY_PATH=/usr/lib64:/lib64;source /usr/cenir/bincenir/freesurfer; mri_convert %s %s',fi{k},fo{k})
-    cmd = sprintf(' mri_convert %s %s',fi{k},fo{k})
+   
+    cmd = sprintf('%s\n mri_convert %s %s',par.version_path,fi{k},fo{k})
   
     %unix(cmd)
   job{end+1} = cmd;
