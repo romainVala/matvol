5527ac5d58fa20e7338c59641891b923be59d1ca
Benoît Béranger <benoit.beranger@icm-institute.org>
2022-02-10 16:36:32 +0100

    job_rp_afni2spm : now can skip & use objects

--- SPM/preproc/job_rp_afni2spm.m
+++ SPM/preproc/job_rp_afni2spm.m
@@ -1,16 +1,58 @@
-function job_rp_afni2spm(dfile, output_dir)
+function job_rp_afni2spm(dfile, output_dir, par)
 
 % Convert the data provided by AFNI (dfile.*1D files) to the same format as SPM data
 % Converted data will be saved in a file called rp_spm.txt at the chosen
 % location (output_dir)
 
 % INPUTS :
-% dfile      = inputs       file, must be cellstr
+% dfile      = inputs       file, must be cellstr or @rp objects
 % output_dir = outputs directory, must be cellstr
+
+
+%% Check input arguments
+
+if ~exist('par','var')
+    par = ''; % for defpar
+end
+
+if nargin < 1
+    help(mfilename)
+end
+
+obj = 0;
+if isa(dfile,'rp')
+    obj = 1;
+    rp_obj = dfile;
+    dfile = rp_obj.to_job();
+end
+
+
+%% defpar
+
+% local parameters
+defpar.outname = 'rp_spm.txt';
+
+% matvol classics
+defpar.sge          = 0;
+defpar.redo         = 0;
+defpar.run          = 1;
+defpar.auto_add_obj = 1;
+
+par = complet_struct(par,defpar);
+
+
+%% main loop
+
+output_file = fullfile(output_dir, par.outname);
 
 N = length(dfile);
 
-for i = 1:N
+for i = 1 : N
+    
+    if exist(output_file{i}, 'file') && ~par.redo
+        fprintf('[%s] skipping %d/%d because exists : %s \n', mfilename, i, N, output_file{i})
+        continue
+    end
     
     % Load text file
     rp_afni = load(dfile{i});
@@ -28,8 +70,7 @@ for i = 1:N
     Datas=[Tx, Ty, Tz, Rx, Ry, Rz]; %#ok<NASGU>
     
     % file .txt
-    filename = fullfile(output_dir{i},'rp_spm.txt');
-    save(filename,'Datas','-ascii','-double','-tabs')
-    fprintf('saving file (%d,%d) : %s \n', i, N, filename)
+    save(output_file{i},'Datas','-ascii','-double','-tabs')
+    fprintf('[%s]: saving file (%d,%d) : %s \n', mfilename, i, N, output_file{i})
     
 end
