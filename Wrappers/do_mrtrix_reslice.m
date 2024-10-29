180abcb719d72053fb81cd7ef84d7418ff4daa1d
valabregue <romain.valabregue@upmc.fr>
2024-10-29 11:46:57 +0100

    make it correct

--- Wrappers/do_mrtrix_reslice.m
+++ Wrappers/do_mrtrix_reslice.m
@@ -1,66 +1,52 @@
-function [ fo, job ] = do_mrtrix_reslice(src,ref,par,jobappend) % prefix,interp_fsl)
-%function fo = do_fsl_reslice(src,ref,prefix)
-%if iscell(prefix) prefix is then the matrix output
+function [ fo, job ] = do_mrtrix_reslice(src,par,jobappend) 
+%function fo = do_mrtrix_reslice(src,par)
 
 if ~exist('par'),par ='';end
 if ~exist('jobappend','var'), jobappend ='';end
 
-
 defpar.prefix = 'r_';
-defpar.interp_fsl = 'trilinear'; % trilinear nearestneighbour sinc spline
-defpar.trans_mat = '';
+defpar.interp = 'cubic'; % nearest,linear, cubic, sinc. Default: cubic)
 defpar.sge = 0;
-defpar.jobname='fsl_reslice';
-defpar.fsl_output_format = 'NIFTI_GZ';
+defpar.jobname='mrtrix_reslice';
 defpar.outfilename='' ;
+defpar.ref = '';
+defpar.voxel = 0;
 
 par = complet_struct(par,defpar);
-
 prefix=par.prefix;
 
 nbj=1;
 
+if isempty(par.outfilename)
+    fo = addprefixtofilenames(src,prefix);
+else
+    fo = par.outfilename;
+end
 
-TODO 
+for k=1:length(src)
 
-if iscell(prefix)
-    for k=1:length(ref)
-        cmd = sprintf('export FSLOUTPUTTYPE=%s;flirt -in %s -ref %s -omat %s  -usesqform -applyxfm -interp %s ',par.fsl_output_format,src{k},ref{k},prefix{k},par.interp_fsl);
+    if ~isempty(par.ref )
+        cmd_opt = sprintf(' -template %s -stride %s ',par.ref{k},par.ref{k})
+    elseif par.voxel
+        cmd_opt = sprintf(' -voxel %f ',par.voxel)
+    else
+        error('you must define either par.template or par.size')
+    end
+    cmd_opt = sprintf('%s -interp %s ', cmd_opt, par.interp);
+
+    ff = cellstr(src{k});
+    ffo = cellstr(fo{k});
+    
+    for kk=1:length(ff)
+
+        cmd = sprintf('mrgrid %s  regrid  %s  %s  ',ff{kk},cmd_opt, ffo{kk});
+        %unix(cmd);
         job{nbj} = cmd;
         nbj=nbj+1;
     end
     
-else
-    
-    if isempty(par.outfilename)
-        fo = addprefixtofilenames(src,prefix);
-    else
-        fo = par.outfilename;
-    end
-    
-    switch par.fsl_output_format
-        case 'NIFTI_GZ'
-            fo=change_file_extension(fo,'.nii.gz');
-    end
-    
-    for k=1:length(ref)
-        ff = cellstr(src{k});
-        ffo = cellstr(fo{k});
-        
-        for kk=1:length(ff)
-            if isempty(par.trans_mat)
-                cmd = sprintf('export FSLOUTPUTTYPE=%s;flirt -in %s -ref %s -out %s  -usesqform -applyxfm  -interp %s ',par.fsl_output_format,ff{kk},ref{k},ffo{kk},par.interp_fsl);
-            else
-                cmd = sprintf('export FSLOUTPUTTYPE=%s;flirt -in %s -ref %s -out %s  -init %s -applyxfm  -interp %s ',par.fsl_output_format,ff{kk},ref{k},ffo{kk},par.trans_mat{k},par.interp_fsl);
-            end
-            %unix(cmd);
-            job{nbj} = cmd;
-            nbj=nbj+1;
-        end
-        
-    end
-    
 end
+
 
 job = do_cmd_sge(job,par,jobappend);
 
